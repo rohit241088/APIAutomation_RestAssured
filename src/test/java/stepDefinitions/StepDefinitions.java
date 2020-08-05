ad37fbee511238a95e1a600e7113952989bfad73
rohit241088 <testerrohitsharma@gmail.com>
2020-08-05 10:47:40 +0530

    resolved compile issue

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -11,7 +11,7 @@ public class StepDefinitions {
 	@Given("User is on home page")
 	public void user_is_on_home_page() {
 	    // Write code here that turns the phrase above into concrete actions
-	response=RequestBuilder.buildRequest("search", "get", "https://www.google.co.in", null, null, null, null);
+	response=RequestBuilder.buildRequest("search", "get", "https://www.google.co.in", null, null, null, null, null);
 		System.out.println();
 	}
 
